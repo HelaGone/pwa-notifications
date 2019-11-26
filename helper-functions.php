99ed4129bfa0b51ab2c541351b0d6463559cec49
Holkan <luah613@gmail.com>
2019-11-26 15:13:29 -0600

    helper funcitons and save manifest in root after reload page. it needs to save it on submit button

---
+++ helper-functions.php
@@ -0,0 +1,54 @@
+<?php
+  /**
+   * [pwa_add_footer_tags] Add amp tags to footer
+   * @param [null]
+   * @return [void]
+  */
+  function pwa_add_footer_tags() {
+    $siteUrl = get_site_url();
+    $options = get_option('pwa_onesignal_option');
+    $appId = $options['pwa_appid_input_field'];
+    echo '<amp-web-push id="amp-web-push" layout="nodislay" helper-iframe-url="'.$siteUrl.'/amp-helper-frame.html?appId='.$appId.'" permission-dialog-url="'.$siteUrl.'/amp-permission-dialog.html?appId='.$appId.'" service-worker-url="'.$siteUrl.'/OneSignalSDKWorker.js?appId='.$appId.'" class="i-amphtml-element i-amphtml-layout-nodisplay" hidden i-amphtml-layout="nodisplay"></amp-web-push>';
+    echo '<amp-install-serviceworker src="'.$siteUrl.'/OneSignalSDKWorker.js?appId='.$appId.'" data-iframe-src="'.$siteUrl.'/install_sw.html" layout="nodisplay" class="i-amphtml-element i-amphtml-layout-nodisplay" hidden i-amphtml-layout="nodisplay">';
+  }
+  add_action('wp_footer', 'pwa_add_footer_tags');
+
+  function pwa_insert_manifest(){
+    $options = get_option('pwa_manifest_option');
+    $fcm_sender_id = $options['pwa_manifest_fcm_sender_id_field'];
+    $background_color = $options['pwa_manifest_bg_color_field'];
+    $theme_color = $options['pwa_manifest_theme_color_field'];
+    $short_name = $options['pwa_manifest_short_name_field'];
+    $manifest = array(
+      "gcm_sender_id_comment" => "For OneSignal web push notifications, Do not change ID",
+      "gcm_sender_id" => $fcm_sender_id,
+      "name" => get_bloginfo('name'),
+      "short_name" => $short_name,
+      "description" => get_bloginfo('description'),
+      "icons" => array(
+        array(
+          "src" => get_template_directory_uri().'/images/icon/192.png',
+          "sizes" => "192x192",
+          "type" => "image/png"
+        ),
+        array(
+          "src" => get_template_directory_uri().'/images/icon/152.png',
+          "sizes" => "152x152",
+          "type" => "image/png"
+        )
+      ),
+      "background_color" => $background_color,
+      "theme_color" => $theme_color,
+      "display" => "standalone",
+      "orientation" => "portrait",
+      "start_url" => "./",
+      "scope" => "./"
+    );
+    $jsonManifest = json_encode($manifest);
+    $fp = fopen('manifest.json', 'w');
+    fwrite($fp, $jsonManifest);
+    fclose($fp);
+  }
+  pwa_insert_manifest();
+
+?>
--- pwa-notifications.php
+++ pwa-notifications.php
@@ -34,6 +34,8 @@ function pwa_plugin_uninstall(){
 }
 register_uninstall_hook(__FILE__, 'pwa_plugin_uninstall');
 
+include_once dirname(__FILE__).'/helper-functions.php';
+
 if(!class_exists('PwaNotifications')){
   class PwaNotifications{
     public function __construct(){
@@ -51,28 +53,59 @@ if(!class_exists('PwaNotifications')){
       add_settings_section('pwa_one_signal_section', 'OneSignal App ID & Rest API Key', array($this, 'pwa_appid_section_callback'), 'pwaOptionsPage');
       add_settings_field('pwa_appid_input_field', 'App ID', array($this, 'pwa_input_apikey_render'), 'pwaOptionsPage', 'pwa_one_signal_section');
       add_settings_field('pwa_restapikey_input_field', 'Rest API Key', array($this, 'pwa_input_restapikey_render'), 'pwaOptionsPage', 'pwa_one_signal_section');
+
+      register_setting('pwaOptionsPage', 'pwa_manifest_option');
+      add_settings_section('pwa_manifest_section', 'Menifest Values', array($this, 'pwa_menifest_section_callback'), 'pwaOptionsPage');
+      add_settings_field('pwa_manifest_bg_color_field', 'Background Color', array($this, 'pwa_render_input_manifest_bg_color'), 'pwaOptionsPage', 'pwa_manifest_section');
+      add_settings_field('pwa_manifest_theme_color_field', 'Theme Color', array($this, 'pwa_render_input_manifest_theme_color'), 'pwaOptionsPage', 'pwa_manifest_section');
+      add_settings_field('pwa_manifest_short_name_field', 'Short Name', array($this, 'pwa_render_input_manifest_short_name'), 'pwaOptionsPage', 'pwa_manifest_section');
+      add_settings_field('pwa_manifest_fcm_sender_id_field', 'FCM Sender ID', array($this, 'pwa_render_input_manifest_fcm_sender_id'), 'pwaOptionsPage', 'pwa_manifest_section');
     }
 
+    //Section callbacks
     public function pwa_appid_section_callback(){
       echo 'Paste the App ID & Rest API Key from OneSignal';
     }
 
-    //Input Fields
+    public function pwa_menifest_section_callback(){
+      echo 'Paste the Background Color, Theme Color, Short Name and FCM Sender ID for the app manifest';
+    }
+
+    //Input Fields for OneSignal Configuration
     public function pwa_input_apikey_render(){
       $options = get_option('pwa_onesignal_option'); ?>
-      <input type="text" name="pwa_onesignal_option[pwa_appid_input_field]" value="<?php echo $options['pwa_appid_input_field']?>" class="custom_input"/>
+      <input type="text" name="pwa_onesignal_option[pwa_appid_input_field]" value="<?php echo $options['pwa_appid_input_field']; ?>" class="custom_input"/>
       <?php
     }
     public function pwa_input_restapikey_render(){
       $options = get_option('pwa_onesignal_option'); ?>
-      <input type="text" name="pwa_onesignal_option[pwa_restapikey_input_field]" value="<?php echo $options['pwa_restapikey_input_field'] ?>" class="custom_input"/>
+      <input type="text" name="pwa_onesignal_option[pwa_restapikey_input_field]" value="<?php echo $options['pwa_restapikey_input_field']; ?>" class="custom_input"/>
       <?php
     }
 
-    public function pwa_restapikey_section_callback(){
-      echo 'Paste the Rest API Key from OneSignal';
+    //Input fields for app Manifest configuration
+    public function pwa_render_input_manifest_bg_color(){
+      $options = get_option('pwa_manifest_option'); ?>
+      <input type="text" name="pwa_manifest_option[pwa_manifest_bg_color_field]" value="<?php echo $options['pwa_manifest_bg_color_field']; ?>" class="custom_input"/>
+      <?php
+    }
+    public function pwa_render_input_manifest_theme_color(){
+      $options = get_option('pwa_manifest_option'); ?>
+      <input type="text" name="pwa_manifest_option[pwa_manifest_theme_color_field]" value="<?php echo $options['pwa_manifest_theme_color_field']; ?>" class="custom_input" />
+      <?php
+    }
+    public function pwa_render_input_manifest_short_name(){
+      $options = get_option('pwa_manifest_option'); ?>
+      <input type="text" name="pwa_manifest_option[pwa_manifest_short_name_field]" value="<?php echo $options['pwa_manifest_short_name_field']; ?>" class="custom_input" />
+      <?php
+    }
+    public function pwa_render_input_manifest_fcm_sender_id(){
+      $options = get_option('pwa_manifest_option'); ?>
+      <input type="text" name="pwa_manifest_option[pwa_manifest_fcm_sender_id_field]" value="<?php echo $options['pwa_manifest_fcm_sender_id_field']; ?>" class="custom_input"/>
+      <?php
     }
 
+    //Plugin Form
     public function pwa_notifications_page(){ ?>
       <form id="pwa_form" action="options.php" method="post" class="">
         <h2>PWA & Notifications</h2>
@@ -82,8 +115,8 @@ if(!class_exists('PwaNotifications')){
           submit_button();
         ?>
       </form>
-    <?php
-    }
+      <?php
+    }//End pwa_notifications_page
 
   }//End class definition
 }//End if class exist
@@ -91,55 +124,5 @@ if(!class_exists('PwaNotifications')){
 if(is_admin()):
 	$options_page = new PwaNotifications();
 endif;
-
-/**
- * [pwa_add_footer_tags] Add amp tags to footer
- * @param [null]
- * @return [void]
-*/
-function pwa_add_footer_tags() {
-  $siteUrl = get_site_url();
-  $options = get_option('pwa_onesignal_option');
-  $appId = $options['pwa_appid_input_field'];
-  echo '<amp-web-push id="amp-web-push" layout="nodislay" helper-iframe-url="'.$siteUrl.'/amp-helper-frame.html?appId='.$appId.'" permission-dialog-url="'.$siteUrl.'/amp-permission-dialog.html?appId='.$appId.'" service-worker-url="'.$siteUrl.'/OneSignalSDKWorker.js?appId='.$appId.'" class="i-amphtml-element i-amphtml-layout-nodisplay" hidden i-amphtml-layout="nodisplay"></amp-web-push>';
-  echo '<amp-install-serviceworker src="'.$siteUrl.'/OneSignalSDKWorker.js?appId='.$appId.'" data-iframe-src="'.$siteUrl.'/install_sw.html" layout="nodisplay" class="i-amphtml-element i-amphtml-layout-nodisplay" hidden i-amphtml-layout="nodisplay">';
-}
-add_action( 'wp_footer', 'pwa_add_footer_tags' );
-
-/**
- * [pwa_insert_manifest]
-*/
-function pwa_insert_manifest(){
-  $manifest = array(
-    "gcm_sender_id_comment" => "For OneSignal web push notifications, Do not change ID",
-    "gcm_sender_id" => "",
-    "name" => get_bloginfo('name'),
-    "short_name" => "",
-    "description" => get_bloginfo('description'),
-    "icons" => array(
-      array(
-        "src" => get_template_directory_uri().'/images/icon/192.png',
-        "sizes" => "192x192",
-        "type" => "image/png"
-      ),
-      array(
-        "src" => get_template_directory_uri().'/images/icon/152.png',
-        "sizes" => "152x152",
-        "type" => "image/png"
-      )
-    ),
-    "background_color" => "#000000",
-    "theme_color" => "#000000",
-    "display" => "standalone",
-    "orientation" => "portrait",
-    "start_url" => "./",
-    "scope" => "./"
-  );
-  $jsonManifest = json_encode($manifest);
-  $fp = fopen('manifest.json', 'w');
-  fwrite($fp, $jsonManifest);
-  fclose($fp);
-}
-pwa_insert_manifest();
 
 ?>
